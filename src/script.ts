6c9bfe32a63d167fe3affd9d716cf00dcc37d661
andrewcargill <110658895+andrewcargill@users.noreply.github.com>
2023-09-25 22:33:42 +0200

    create timer with start, stop and reset buttons

---
+++ src/script.ts
@@ -0,0 +1,57 @@
+let intervalId: number | null = null;
+let startTime: number = 0;
+
+const timerDisplay: HTMLDivElement | null = document.getElementById('timer') as HTMLDivElement | null;
+const startButton: HTMLButtonElement | null = document.getElementById('startButton') as HTMLButtonElement | null;
+const stopButton: HTMLButtonElement | null = document.getElementById('stopButton') as HTMLButtonElement | null;
+
+function updateTimer() {
+    if (timerDisplay !== null) {
+        const currentTime: number = Date.now();
+        console.log(currentTime);
+        const elapsedTime: number = currentTime - startTime;
+        const minutes: number = Math.floor(elapsedTime / 60000);
+        console.log(minutes);
+        const seconds: number = Math.floor((elapsedTime % 60000) / 1000);
+        console.log(seconds);
+        const display: string = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        timerDisplay.textContent = display;
+    }
+}
+
+if (startButton !== null && stopButton !== null && timerDisplay !== null) {
+    startButton.addEventListener('click', () => {
+        if (intervalId === null) {
+            startTime = Date.now(); // Set startTime to the current time
+            intervalId = window.setInterval(updateTimer, 1000);
+        }
+    });    
+
+    stopButton.addEventListener('click', () => {
+        if (intervalId !== null) {
+            console.log('stop button clicked')
+            window.clearInterval(intervalId);
+            intervalId = null;
+        }
+    });
+
+    updateTimer(); // Initialize the timer display
+}
+
+const resetButton: HTMLButtonElement | null = document.getElementById('resetButton') as HTMLButtonElement | null;
+
+function resetTimer() {
+    // Clear the interval and reset timer values
+    if (intervalId !== null) {
+        window.clearInterval(intervalId);
+        intervalId = null;
+    }
+    startTime = Date.now();
+    console.log('Reset Called!')
+    updateTimer(); // Update the timer display
+}
+
+// Event listener for the Reset button
+if (resetButton !== null) {
+    resetButton.addEventListener('click', resetTimer);
+}
